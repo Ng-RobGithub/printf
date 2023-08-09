f89389a08ceb9504af9c6da26b3b92abb7a8e9d6
ngrob4real@gmail.com <Ng-RobGithub>
2023-08-08 18:16:13 -0700

    a function that produces output

--- _printf.c
+++ _printf.c
@@ -36,4 +36,24 @@ int _printf(const char *format, ...)
 
 	return (count);
 }
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @size: represents the size.
+ */
+void print_buffer(const char *buffer, size_t size)
+{
+	size_t i;
 
+	if (buffer != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("%c", buffer[i]);
+		}
+		printf("\n");
+	} else
+	{
+		printf("Buffer is empty or invalid.\n");
+	}
+}
